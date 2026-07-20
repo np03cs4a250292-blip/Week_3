f38dfc69a3b95d6b07e606d5005a517ee400e074
np03cs4a250292-blip <np03cs4a250292@heraldcollege.edu.np>
2026-07-20 19:01:41 +0545

    Apply css through className in TaskContainer.jsx

--- src/components/TaskContainer/TaskContainer.jsx
+++ src/components/TaskContainer/TaskContainer.jsx
@@ -6,24 +6,28 @@ function TaskContainer() {
     { time: "9:00 AM", text: "Get eggs", isUrgent: true },
     { time: "9:05 AM", text: "Clean your room", isUrgent: false },
     { time: "10:00 AM", text: "Complete task 1", isUrgent: false },
-    { time: "4:00 PM", text: "Go for a walk", isUrgent: true },
+    { time: "4:00 PM", text: "Go for a walk", isUrgent: false },
   ];
 
-  return (
-    <>
-      <h2>{containerTitle}</h2>
-      <ul>
-        {tasks.map((task, index) => (
-          <li
-            className={task.isUrgent ? "task-item urgent-task" : "task-item"}
-            key={index}
-          >
-            <span>{task.time}</span>-<span>{task.text}</span>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  if (tasks.length === 0) {
+    return <h2>No Pending Tasks</h2>;
+  } else {
+    return (
+      <>
+        <h2>{containerTitle}</h2>
+        <ul>
+          {tasks.map((task, index) => (
+            <li
+              className={task.isUrgent ? "task-item urgent-task" : "task-item"}
+              key={index}
+            >
+              <span>{task.time}</span>-<span>{task.text}</span>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default TaskContainer;
