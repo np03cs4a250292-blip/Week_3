70ddd68dfb6b1e2f06c8d7c9f9958bca4d92fd76
np03cs4a250292-blip <np03cs4a250292@heraldcollege.edu.np>
2026-07-20 19:07:27 +0545

    Conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import PageTitle from "./components/PageTitle/PageTitle";
+import TaskContainer from "./components/TaskContainer/TaskContainer";
 
 function App() {
   return (
     <>
       <PageTitle />
+      <TaskContainer />
     </>
   );
 }
