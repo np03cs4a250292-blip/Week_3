101a3f3402977978382092ca0dbb2ff65ac9d1e8
np03cs4a250292-blip <np03cs4a250292@heraldcollege.edu.np>
2026-07-20 18:57:23 +0545

    Apply css through className in TaskContainer.jsx

--- src/components/TaskContainer/TaskContainer.jsx
+++ src/components/TaskContainer/TaskContainer.jsx
@@ -1,21 +1,23 @@
 import "./TaskContainer.css";
 
 function TaskContainer() {
-  const containerTitle = "Task Due Today";
+  const containerTitle = "Tasks Due Today";
   const tasks = [
     { time: "9:00 AM", text: "Get eggs", isUrgent: true },
     { time: "9:05 AM", text: "Clean your room", isUrgent: false },
-    { time: "10:00 AM", text: "Complete task1", isUrgent: false },
-    { time: "4:00 PM", text: "Go for a walk", isUrgent: false },
+    { time: "10:00 AM", text: "Complete task 1", isUrgent: false },
+    { time: "4:00 PM", text: "Go for a walk", isUrgent: true },
   ];
 
-  const listItemStyle = {listStyle: "none", color: "whitesmoke", fontSize: "1rem"};
   return (
     <>
       <h2>{containerTitle}</h2>
       <ul>
         {tasks.map((task, index) => (
-          <li style={listItemStyle} key={index}>
+          <li
+            className={task.isUrgent ? "task-item urgent-task" : "task-item"}
+            key={index}
+          >
             <span>{task.time}</span>-<span>{task.text}</span>
           </li>
         ))}
