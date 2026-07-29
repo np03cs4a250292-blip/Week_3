aa4ab486331c20f0778359fd7ac7d2a3d3132859
np03cs4a250292-blip <np03cs4a250292@heraldcollege.edu.np>
2026-07-29 19:42:40 +0545

    write content to the file(Week4, Task1)

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+fs.writeFile("output.txt", "Hello, world", (err) => {
+  if (err) {
+    console.error("Error writing file:", err);
+  }
+
+  console.log("File written successfully");
+});
