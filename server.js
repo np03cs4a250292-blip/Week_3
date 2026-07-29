ba87b05314e4e1d8e3ec3c3ef903bc623ae596a0
np03cs4a250292-blip <np03cs4a250292@heraldcollege.edu.np>
2026-07-29 21:07:45 +0545

    requests to the root route /

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   // Route handling will go here
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to my Node.js Server!");
+  }
 });
 
 server.listen(PORT, () => {
