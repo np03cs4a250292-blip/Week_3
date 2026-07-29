500c4d2bc19e10c96c59745b079bb941e6dc2540
np03cs4a250292-blip <np03cs4a250292@heraldcollege.edu.np>
2026-07-29 21:15:15 +0545

    Handle unknown Routes

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ const server = http.createServer((req, res) => {
     };
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(student));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 - Page Not Found");
   }
 });
 
