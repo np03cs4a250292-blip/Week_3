f5bdea5f833077431524b20d7a05571860c2fd59
np03cs4a250292-blip <np03cs4a250292@heraldcollege.edu.np>
2026-08-06 07:26:20 +0545

    Add conditional rendering in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-// import PageTitle from "./components/PageTitle/PageTitle";
+import { useState } from "react";
+import PageTitle from "./components/PageTitle/PageTitle";
 import TaskContainer from "./components/TaskContainer/TaskContainer";
+import Form from "./components/Form/Form";
+import PageHeader from "./components/PageHeader/PageHeader";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(0);
   const containerTitle = "Tasks Due Today";
   const tasks = [
     { time: "9:00 AM", text: "Get eggs", isUrgent: true },
     { time: "9:05 AM", text: "Clean your room", isUrgent: false },
     { time: "10:00 AM", text: "Complete task 1", isUrgent: false },
-    { time: "4:00 PM", text: "Go for a walk", isUrgent: false },
+    { time: "4:00 PM", text: "Go for a walk", isUrgent: true },
   ];
+
   return (
     <>
-      {/* <PageTitle /> */}
-      <TaskContainer containerTitle={containerTitle} tasks={tasks} />
-      {/* <TaskContainer /> */}
+      <PageTitle />
+      <PageHeader currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {currentPage === 1 && <Form />}
+      {currentPage === 0 && (
+        <TaskContainer containerTitle={containerTitle} tasks={tasks} />
+      )}
     </>
   );
 }
